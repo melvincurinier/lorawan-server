b6dd8d4eda2e6e58c86b226527d1f0af4f412c4f
Melvin Curinier <mcurinier@outlook.com>
2024-07-04 10:31:18 +0200

    comments mysql.js

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 
 const { logDatabase } = require('../util/coloredLog');
 
+// Create a connection pool to the MySQL database
 const mysqldb = mysql.createPool({
     host: process.env.MYSQL_HOSTNAME,
     port: process.env.MYSQL_PORT,
@@ -10,23 +11,32 @@ const mysqldb = mysql.createPool({
     database: process.env.MYSQL_DBNAME,
 });
 
+/**
+ * A function that check the database connection
+ */
 async function checkConnection() {
     try{
+        // Attempt to get a connection from the pool
         const connection = await mysqldb.getConnection();
+
         logDatabase('Database connection established', false);
+
+        // Release the connection back to the pool
         connection.release();
     } catch (error) {
+        // Handle specific connection errors and log appropriate messages
         if (error.code === 'PROTOCOL_CONNECTION_LOST') {
             logDatabase('Database connection was closed', true);
         } else if (error.code === 'ER_CON_COUNT_ERROR') {
             logDatabase('Database has too many connections', true);
-        }
-        if (error.code === 'ECONNREFUSED') {
+        } else if (error.code === 'ECONNREFUSED') {
             logDatabase('Database connection was refused', true);
         }
     }
 }
 
+// Check the status of database connection
 checkConnection();
 
+// Export the connection pool for use in other modules
 module.exports = mysqldb;
