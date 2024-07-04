9a44f36717b257a6d2703bd2340cdb671dcbd4e6
Melvin Curinier <mcurinier@outlook.com>
2024-07-04 11:21:55 +0200

    comments broker.js

--- server/config/broker.js
+++ server/config/broker.js
@@ -1,67 +1,81 @@
-require('dotenv').config({ path: '../.env' });
-
 const { logBroker } = require('../util/coloredLog');
 
+/**
+ * A function that authenticate the client to the aedes broker
+ */
+function authenticateClient(client, username, password, callback){
+  // Convert password to string if it exists
+  let passwordToString = '';
+  if(password) passwordToString = password.toString();
+
+  // Check if authentication details are provided in environment variables
+  if (process.env.AEDES_AUTH_USERNAME && process.env.AEDES_AUTH_PASSWORD) {
+    // Authenticate the client
+    if (username === process.env.AEDES_AUTH_USERNAME && passwordToString === process.env.AEDES_AUTH_PASSWORD) {
+      logBroker(`Client Authenticated ${client.id}`, false);
+      callback(null, true);
+      return;
+    }
+    logBroker(`Client Not Authenticated ${client.id}`, false);
+    const error = new Error("Auth error");
+    error.returnCode = 4;
+    callback(error, null);
+    return;
+  }
+  logBroker(`Zero Auth ${client.id}`, false);
+  callback(null, true);
+  return;
+}
+
+// Initialize the Aedes MQTT broker with configuration options
 const aedes = require('aedes')({
   id: process.env.AEDES_ID,
   concurrency: process.env.CONCURRENCY || 10000,
   queueLimit: process.env.QUEUE_LIMIT || 100,
   connectTimeout: process.env.CONNECTION_TIMEOUT || 30000,
   authenticate: (client, username, password, callback) => {
-    let passwordToString = '';
-    if(password) passwordToString = password.toString();
-    if (process.env.AEDES_AUTH_USERNAME && process.env.AEDES_AUTH_PASSWORD) {
-      if (username === process.env.AEDES_AUTH_USERNAME && passwordToString === process.env.AEDES_AUTH_PASSWORD) {
-        logBroker(`Client Authenticated ${client.id}`, false);
-        callback(null, true);
-        return;
-      }
-      logBroker(`Client Not Authenticated ${client.id}`, false);
-      const error = new Error("Auth error");
-      error.returnCode = 4;
-      callback(error, null);
-      return;
-    }
-    logBroker(`Zero Auth ${client.id}`, false);
-    callback(null, true);
-    return;
+    authenticateClient(client, username, password, callback);
   }
 });
 
-// emitted when a client connects to the broker
+// Event emitted when a client connects to the broker
 aedes.on('client', function (client) {
   logBroker(`CLIENT_CONNECTED : MQTT Client ${(client ? client.id : client)} connected to aedes broker ${aedes.id}`, false)
 })
 
-// emitted when a client disconnects from the broker
+// Event emitted when a client disconnects from the broker
 aedes.on('clientDisconnect', function (client) {
   logBroker(`CLIENT_DISCONNECTED : MQTT Client ${(client ? client.id : client)} disconnected from the aedes broker ${aedes.id}`, false)
 })
 
-// emitted when a client subscribes to a message topic
+// Event emitted when a client subscribes to a message topic
 aedes.on('subscribe', function (subscriptions, client) {
   logBroker(`TOPIC_SUBSCRIBED : MQTT Client ${(client ? client.id : client)} subscribed to topic: ${subscriptions.map(s => s.topic).join(',')} on aedes broker ${aedes.id}`, false)
 })
 
-// emitted when a client unsubscribes from a message topic
+// Event emitted when a client unsubscribes from a message topic
 aedes.on('unsubscribe', function (subscriptions, client) {
   logBroker(`TOPIC_UNSUBSCRIBED : MQTT Client ${(client ? client.id : client)} unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${aedes.id}`, false)
 })
 
-// emitted when a client publishes a message packet on the topic
+// Event emitted when a client publishes a message packet on the topic
 aedes.on('publish', function (packet, client) {if (client) {
   logBroker(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`, false)}
 })
 
+// Check if the Aedes broker is initialized
 if(!aedes.id){
   logBroker('Aedes not initialized', false);
   return;
 }
 
+// Log that Aedes is initialized
 logBroker(`Aedes initialized: id ${aedes.id}`,false);
 
+// Create a TCP server to handle MQTT connections
 const server = require('net').createServer(aedes.handle);
 
+// Start the server and listen on the specified port
 server.listen(process.env.MQTT_PORT, () => {
   logBroker(`MQTT broker started and listening on port ${process.env.MQTT_PORT}`, false);
 });
