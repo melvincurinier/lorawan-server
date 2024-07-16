3202479c75248666c3ebc8d7db9a6d31e6cc1b61
Melvin Curinier <mcurinier@outlook.com>
2024-07-16 15:19:25 +0200

    add comments ftp.js & ftpController.js

--- server/config/ftp.js
+++ server/config/ftp.js
@@ -1,76 +1,78 @@
+// Import modules
 const FtpSrv = require('ftp-srv');
-const fs = require('fs');
-const path = require('path');
-
 const { networkInterfaces } = require('os');
 const { Netmask } = require('netmask');
-
 const { parseCSV } = require('../controllers/ftpController');
 
+/**
+ * A function that get network interfaces and their addresses
+ */
 function getNetworks() {
   const nets = networkInterfaces();
   let networks = {};
   for (const name of Object.keys(nets)) {
       for (const net of nets[name]) {
           if (net.family === 'IPv4' && !net.internal) {
-              networks[net.address + "/24"] = net.address
+              networks[net.address + "/24"] = net.address // Store address with subnet mask
           }
       }
   }
   return networks;
 }
 
+/**
+ * A function to resolve the passive FTP URL based on the client's address
+ */
 const resolverFunction = (address) => {
   const networks = getNetworks();
   for (const network in networks) {
       if (new Netmask(network).contains(address)) {
-          return networks[network];
+          return networks[network]; // Return the matching network address
       }
   }
-  return "127.0.0.1";
+  return "127.0.0.1"; // Return localhost if no match
 }
 
-const ftp_url = process.env.FTP_HOSTNAME;
-const ftp_port = process.env.FTP_PORT;
-
+// Create a new FTP server instance
 const ftpServer = new FtpSrv({
-  url: `ftp://${ftp_url}:${ftp_port}`,
-  pasv_url: resolverFunction
+  url: `ftp://${process.env.FTP_HOSTNAME}:${process.env.FTP_PORT}`,
+  pasv_url: resolverFunction // Passive mode URL resolver function
 });
 
+// Event listener for login attempts
 ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
   if (username === process.env.FTP_AUTH_USERNAME && password === process.env.FTP_AUTH_PASSWORD) {
-    resolve({ root: './data' }); // Répertoire racine pour les utilisateurs autorisés
+    resolve({ root: './data' }); // Root directory for authorized users
     console.log(`User ${username} logged in`);
   } else {
-    reject(new Error('Invalid username or password'));
+    reject(new Error('Invalid username or password')); // Reject if authentication fails
   }
 
+   // Event listener for file storage
   connection.on('STOR', async (error, stream) => { 
     if(error){
       console.error('Error storing file: ', error);
       return;
     }
-    console.log(`File stored : ${stream}`);
+    console.log(`File stored: ${stream}`);
 
-    try{
-      
-    } catch (error) {
-      console.error('Error processing CSV file:', error);
-    }
+    const results = await parseCSV(stream);
     
   });
 });
 
+// Event listener for disconnections
 ftpServer.on('disconnect', ({connection}) => {
   const username = connection.username;
   console.log(`User ${username} disconnected`);
 });
 
+// Event listener for server errors
 ftpServer.on('error', (error) => {
   console.error('FTP Server error:', error);
 });
 
+// Start the FTP server
 ftpServer.listen()
   .then(() => {
       console.log('FTP server is running on port 21');
@@ -78,6 +80,3 @@ ftpServer.listen()
   .catch(error => {
     console.error(`Error starting FTP server: ${error.message}`);
   });
-
-const results = parseCSV("./data/ftp/DATA/SITE_1810C4_I_30@9_Index_2024-07-15_13-45-00_2024-07-15_13-55-00_UTC0-00.csv");
-console.log('Parsed CSV results:', results);
--- server/controllers/ftpController.js
+++ server/controllers/ftpController.js
@@ -1,26 +1,34 @@
+// Import modules
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
+/**
+ * A function that parse a CSV file and return the data.
+ */
 const parseCSV = (filePath) => {
   return new Promise((resolve, reject) => {
-    const results = [];
+    const results = []; // Array to hold the parsed data
 
+    // Create a readable stream from the CSV file
     fs.createReadStream(filePath, 'utf-8')
+      // Pipe the stream through the CSV parser with the specified options
       .pipe(parse({ 
-        delimiter: ',', 
-        columns: true,
-        record_delimiter: ',\r',
-        relax_column_count_more: true,
-        skip_empty_lines: true
+        delimiter: ',', // Comma as the column delimiter
+        columns: true, // Use the first row as header to generate object keys
+        record_delimiter: ',\r\n', // Define the line delimiter
+        relax_column_count: true, // Discard inconsistent columns
+        skip_empty_lines: true, // Ignore empty lines in the CSV file
       }))
+      // Event listener for each data row
       .on('data', (row) => {
         results.push(row);
       })
+      // Event listener for the end of the stream
       .on('end', () => {
         console.log('CSV file successfully processed');
-        console.log('Data:', results);
         resolve(results);
       })
+      // Event listener for errors
       .on('error', (error) => {
         console.error(`Error reading CSV file: ${filePath}`, error);
         reject(error);
@@ -28,4 +36,5 @@ const parseCSV = (filePath) => {
   });
 };
 
+// Export the functions for use in other modules
 module.exports = { parseCSV };
