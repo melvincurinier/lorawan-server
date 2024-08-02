4a43e8d6731df9280d70f4e55956bf616ac4ff98
install <install@vm-energie.cinam.univ-mrs.fr>
2024-08-02 16:31:23 +0200

    ftp meilleure ecoute serveur port & log modif

--- config/ftp.js
+++ config/ftp.js
@@ -6,7 +6,6 @@ const FtpSrv = require('ftp-srv');
 const { networkInterfaces } = require('os');
 const { Netmask } = require('netmask');
 const { addSocomecDataFromStream } = require('../controllers/socomecFtpController');
-const { logFTP } = require('../util/coloredLog');
 const { checkDbConnection } = require('./mysql');
 
 /**
@@ -42,52 +41,61 @@ const resolverFunction = (address) => {
 const ftp_url = process.env.FTP_HOSTNAME;
 const ftp_port = process.env.FTP_PORT;
 
-// Create a new FTP server instance
-const ftpServer = new FtpSrv({
-  url: `ftp://${ftp_url}:${ftp_port}`,
-  pasv_url: resolverFunction // Passive mode URL resolver function
-});
-
-// Event listener for login attempts
-ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
-  if (username === process.env.FTP_AUTH_USERNAME && password === process.env.FTP_AUTH_PASSWORD) {
-    resolve({ root: './data' }); // Root directory for authorized users
-    logFTP(`User ${username} logged in`, false);
-  } else {
-    reject(new Error('Invalid username or password')); // Reject if authentication fails
-  }
-
-   // Event listener for file storage
-  connection.on('STOR', (error, stream) => { 
-    if(error){
-      logFTP(`Error storing file: ${error}`, true);
-      return;
-    }
-    logFTP(`File stored: ${stream}`, false);
-
-    addSocomecDataFromStream(stream)
+const startFTPServer = () => {
+  // Create a new FTP server instance
+  const ftpServer = new FtpSrv({
+    url: `ftp://${ftp_url}:${ftp_port}`,
+    pasv_url: resolverFunction // Passive mode URL resolver function
   });
-});
 
-// Event listener for disconnections
-ftpServer.on('disconnect', ({connection}) => {
-  const username = connection.username;
-  logFTP(`User ${username} disconnected`, false);
-});
+  // Event listener for login attempts
+  ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
+    if (username === process.env.FTP_AUTH_USERNAME && password === process.env.FTP_AUTH_PASSWORD) {
+      resolve({ root: './data' }); // Root directory for authorized users
+      console.log(`User ${username} logged in`);
+    } else {
+      reject(new Error('Invalid username or password')); // Reject if authentication fails
+    }
 
-// Event listener for server errors
-ftpServer.on('error', (error) => {
-  logFTP(`FTP Server error: ${error}`, true);
-});
+    // Event listener for file storage
+    connection.on('STOR', (error, stream) => { 
+      if(error){
+        console.error(`Error storing file: ${error}`);
+        return;
+      }
+      console.log(`File stored: ${stream}`);
 
-// Start the FTP server
-ftpServer.listen()
-.then(() => {
-  logFTP(`FTP server is running on port ${ftp_port}`, false);
-})
-.catch(error => {
-  logFTP(`Error starting FTP server: ${error.message}`, true);
-});
+      addSocomecDataFromStream(stream);
+    });
+  });
+
+  // Event listener for disconnections
+  ftpServer.on('disconnect', ({connection}) => {
+    const username = connection.username;
+    console.log(`User ${username} disconnected`);
+  });
+
+  // Event listener for server errors
+  ftpServer.on('error', (error) => {
+    console.error(`FTP Server error: ${error}`);
+    console.log('Retrying in 5 seconds...');
+    setTimeout(startFTPServer, 5000); // Retry after 5 seconds
+  });
+
+  // Start the FTP server
+  ftpServer.listen()
+    .then(() => {
+      console.log(`FTP server is running on port ${ftp_port}`);
+    })
+    .catch(error => {
+      console.error(`Error starting FTP server: ${error.message}`);
+      console.log('Retrying in 5 seconds...');
+      setTimeout(startFTPServer, 5000); // Retry after 5 seconds
+    });
+}
+
+// Start the FTP server for the first time
+startFTPServer();
 
 // Check the status of database connection
 checkDbConnection();
