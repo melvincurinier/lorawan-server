6bc7233b84e9bc0784fb25c2acaa243ec60b23b7
install <install@vm-energie.cinam.univ-mrs.fr>
2024-08-02 15:42:14 +0200

    meilleure ecoute serveur port & log modif

--- config/broker.js
+++ config/broker.js
@@ -1,9 +1,6 @@
 // Load environment variables from .env file
 require('dotenv').config();
 
-// Import module
-const { logBroker } = require('../util/coloredLog');
-
 /**
  * A function that authenticate the client to the aedes broker
  */
@@ -16,17 +13,17 @@ function authenticateClient(client, username, password, callback){
   if (process.env.AEDES_AUTH_USERNAME && process.env.AEDES_AUTH_PASSWORD) {
     // Authenticate the client
     if (username === process.env.AEDES_AUTH_USERNAME && passwordToString === process.env.AEDES_AUTH_PASSWORD) {
-      logBroker(`Client Authenticated ${client.id}`, false);
+      console.log(`Client Authenticated ${client.id}`);
       callback(null, true);
       return;
     }
-    logBroker(`Client Not Authenticated ${client.id}`, false);
+    console.error(`Client Not Authenticated ${client.id}`);
     const error = new Error("Auth error");
     error.returnCode = 4;
     callback(error, null);
     return;
   }
-  logBroker(`Zero Auth ${client.id}`, false);
+  console.error(`Zero Auth ${client.id}`);
   callback(null, true);
   return;
 }
@@ -41,42 +38,55 @@ const aedes = require('aedes')({
 
 // Event emitted when a client connects to the broker
 aedes.on('client', function (client) {
-  logBroker(`CLIENT_CONNECTED : MQTT Client ${(client ? client.id : client)} connected to aedes broker ${aedes.id}`, false)
+  console.log(`CLIENT_CONNECTED : MQTT Client ${(client ? client.id : client)} connected to aedes broker ${aedes.id}`)
 })
 
 // Event emitted when a client disconnects from the broker
 aedes.on('clientDisconnect', function (client) {
-  logBroker(`CLIENT_DISCONNECTED : MQTT Client ${(client ? client.id : client)} disconnected from the aedes broker ${aedes.id}`, false)
+  console.log(`CLIENT_DISCONNECTED : MQTT Client ${(client ? client.id : client)} disconnected from the aedes broker ${aedes.id}`)
 })
 
 // Event emitted when a client subscribes to a message topic
 aedes.on('subscribe', function (subscriptions, client) {
-  logBroker(`TOPIC_SUBSCRIBED : MQTT Client ${(client ? client.id : client)} subscribed to topic: ${subscriptions.map(s => s.topic).join(',')} on aedes broker ${aedes.id}`, false)
+  console.log(`TOPIC_SUBSCRIBED : MQTT Client ${(client ? client.id : client)} subscribed to topic: ${subscriptions.map(s => s.topic).join(',')} on aedes broker ${aedes.id}`)
 })
 
 // Event emitted when a client unsubscribes from a message topic
 aedes.on('unsubscribe', function (subscriptions, client) {
-  logBroker(`TOPIC_UNSUBSCRIBED : MQTT Client ${(client ? client.id : client)} unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${aedes.id}`, false)
+  console.log(`TOPIC_UNSUBSCRIBED : MQTT Client ${(client ? client.id : client)} unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${aedes.id}`)
 })
 
 // Event emitted when a client publishes a message packet on the topic
 aedes.on('publish', function (packet, client) {if (client) {
-  logBroker(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`, false)}
+  console.log(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`)}
 })
 
-// Check if the Aedes broker is initialized
-if(!aedes.id){
-  logBroker('Aedes not initialized', false);
-  return;
-}
+const startServer = () => {
+  // Check if the Aedes broker is initialized
+  if (aedes.id) {
+    // Log that Aedes is initialized
+    console.log(`Aedes initialized: id ${aedes.id}`);
 
-// Log that Aedes is initialized
-logBroker(`Aedes initialized: id ${aedes.id}`,false);
+    // Create a server to handle MQTT connections
+    const server = require('net').createServer(aedes.handle);
 
-// Create a TCP server to handle MQTT connections
-const server = require('net').createServer(aedes.handle);
+    // Try to start the server and listen on the specified port
+    server.listen(process.env.MQTT_PORT, () => {
+      console.log(`MQTT broker started and listening on port ${process.env.MQTT_PORT}`);
+    });
 
-// Start the server and listen on the specified port
-server.listen(process.env.MQTT_PORT, () => {
-  logBroker(`MQTT broker started and listening on port ${process.env.MQTT_PORT}`, false);
-});
+    // Handle server errors
+    server.on('error', (err) => {
+      console.error(`Server error: ${err.message}`);
+      console.log('Retrying in 5 seconds...');
+      setTimeout(startServer, 5000); // Retry after 5 seconds
+    });
+  } else {
+    console.error('Aedes not initialized');
+    console.log('Retrying in 5 seconds...');
+    setTimeout(startServer, 5000); // Retry after 5 seconds
+  }
+};
+
+// Start the server for the first time
+startServer();
